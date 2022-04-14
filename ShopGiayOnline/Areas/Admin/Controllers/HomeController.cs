4f3f5d4e2751f6b3be390e823b360c9c3cd16980
SungThanCong <blueskylove2002@gmail.com>
2022-04-14 14:00:26 +0700

    Add project files.

---
+++ ShopGiayOnline/Areas/Admin/Controllers/HomeController.cs
@@ -0,0 +1,58 @@
+﻿using ShopGiayOnline.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace ShopGiayOnline.Areas.Admin.Controllers
+{
+    public class HomeController : Controller
+    {
+        // GET: Admin/Home
+        private SHOPGIAYEntities db = new SHOPGIAYEntities();
+        public ActionResult Index()
+        {
+            if(Session["username"] != null)
+            {
+                return View();
+            }
+            else
+            {
+                return RedirectToAction("Login");
+            }
+        }
+
+        public ActionResult Login()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Login(string tendangnhap, string matkhau)
+        {
+            if (ModelState.IsValid)
+            {
+                var data = db.TAIKHOANCHUs.Where(s => s.tendangnhap.Equals(tendangnhap) && s.matkhau.Equals(matkhau)).ToList();
+                if (data.Count() > 0)
+                {
+                    //add session
+                    Session["username"] = data.FirstOrDefault().tendangnhap;
+                    Session["password"] = data.FirstOrDefault().matkhau;
+                    return RedirectToAction("Index");
+                }
+                else
+                {
+                    ViewBag.error = "Login failed";
+                }
+            }
+            return View();
+        }
+        public ActionResult Logout()
+        {
+            Session.Clear();//remove session
+            return RedirectToAction("Login");
+        }
+    }
+}
